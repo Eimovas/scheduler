ee7d654f3e4bda325085803092ed25ccb9809c67
Eimantas Stankevicius <estankevicius@live.com>
2019-03-08 10:57:11 +0800

    Initial commit with paket - everything works for now.

---
+++ Scheduler.Core/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Scheduler.Core
+
+module List = 
+    let flatten (listOfLists: 'a list list ) : 'a list = 
+        listOfLists |> List.fold (fun aggr current -> List.append current aggr) []
